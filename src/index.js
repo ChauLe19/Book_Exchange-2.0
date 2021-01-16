b5c1ec3a24e50a99368b05432e065b04d19e99e4
aditi977 <aditid@vt.edu>
2021-01-16 10:52:34 -0500

    starting to change initial

--- src/index.js
+++ src/index.js
@@ -26,12 +26,33 @@ import {
     Route,
     Link
 } from "react-router-dom"
-
+import SignIn from './components/SignIn'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import {blue} from '@material-ui/core/colors'
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:2000/',
     credentials: "same-origin",
 })
+
+const colorTheme = createMuiTheme({
+    palette: {
+      primary: {
+        // light: will be calculated from palette.primary.main,
+        main: blue[600],
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contrast with palette.primary.main
+      },
+      secondary: {
+        light: '#f73378',
+        main: '#f50057',
+        // dark: will be calculated from palette.secondary.main,
+        contrastText: '#fff',
+      },
+      contrastThreshold: 3,
+      tonalOffset: 0.2,
+    }, 
+  });
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem("token");
@@ -66,35 +87,40 @@ class App extends Component {
     render() {
 
         return (
-            <Router>
-                <ApolloProvider client={client}>
+            <div>
+                <ThemeProvider theme={colorTheme}>
+                    <SignIn/>
+                </ThemeProvider>
+            </div>
+            // <Router>
+            //     <ApolloProvider client={client}>
                     
-                    <Header isLogin={this.state.authToken} handleLogin={this.handleLogin}/>
-                    <Switch>
+            //         <Header isLogin={this.state.authToken} handleLogin={this.handleLogin}/>
+            //         <Switch>
 
-                        <Route path="/search">
-                            <h2>SearchPage</h2>
-                            <SearchPage />
-                        </Route>
-                        <Route exact path="/forSaleBooks/:bookId">
-                            <h2>Book Page</h2>
-                            <BookPage />
-                        </Route>
-                        <Route path="/my">
-                            <h2>My shelves</h2>
-                            <MyShelves />
-                        </Route>
-                        <Route path="/book/isbn">
-                            <h2>Book Info</h2>
-                            <BookInfo />
-                        </Route>
-                        <Route exact path="/login" render={(props)=><LoginPage handleLogin={this.handleLogin} props={props}/>}/>
-                        <Route exact path="/user/sell" render={()=><SellPage />}/>
-                        <Route exact path="/register" render={(props)=><LoginPage handleLogin={this.handleLogin} props={props}/>}/>
-                        <Route exact path="/" render={(props)=><Home history={props.history}/>}/>
-                    </Switch>
-                </ApolloProvider>
-            </Router>
+            //             <Route path="/search">
+            //                 <h2>SearchPage</h2>
+            //                 <SearchPage />
+            //             </Route>
+            //             <Route exact path="/forSaleBooks/:bookId">
+            //                 <h2>Book Page</h2>
+            //                 <BookPage />
+            //             </Route>
+            //             <Route path="/my">
+            //                 <h2>My shelves</h2>
+            //                 <MyShelves />
+            //             </Route>
+            //             <Route path="/book/isbn">
+            //                 <h2>Book Info</h2>
+            //                 <BookInfo />
+            //             </Route>
+            //             <Route exact path="/login" render={(props)=><LoginPage handleLogin={this.handleLogin} props={props}/>}/>
+            //             <Route exact path="/user/sell" render={()=><SellPage />}/>
+            //             <Route exact path="/register" render={(props)=><LoginPage handleLogin={this.handleLogin} props={props}/>}/>
+            //             <Route exact path="/" render={(props)=><Home history={props.history}/>}/>
+            //         </Switch>
+            //     </ApolloProvider>
+            // </Router>
         )
     }
 }
