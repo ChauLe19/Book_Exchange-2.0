43e709156acdf59c81b69bb31448a83982061e07
aditi977 <aditid@vt.edu>
2021-02-14 13:08:00 -0500

    fixed navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,40 +3,27 @@ import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Link from 'next/link'
 
 const NavBar = () => {
     return (
         <div id="navbar" color="text.primary">
+            <CssBaseline/>
             <AppBar position="static" >
-                <ToolBar>
-                    <Typography variant="h5" color="inherit">
+                <ToolBar style={{margin: 0}}>
+                    <Typography variant="h5" color="inherit" style={{ flex: 1 }}>
                         Book Trader
                     </Typography>
-                    <ul className="inline-list">
-                        <li>
-                            <Link href="/">
-                                Home
-                                {/* <Button variant="text" size="large" color="primary" href="/"> Home</Button> */}
-                            </Link>
-
-                        </li>
-                        <li>
-                            <Link href="/login">
-                                Login
-                            {/* <Button size="large" color="primary" href="/login"> Login</Button> */}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/my/book-shelves">
-                                <a>
-
-                                My shelves
-                                </a>
-                                {/* <Button size="large" color="primary" href="/my/book-shelves"> My Shelves</Button> */}
-                            </Link>
-                        </li>
-                    </ul>
+                    <Link href="/" passHref>
+                        <Button variant="contained" color="primary" size="large" disableElevation>Home</Button>
+                    </Link>
+                    <Link href="/login" passHref>
+                        <Button variant="contained" color="primary" size="large" disableElevation>Login</Button>
+                    </Link>
+                    <Link href="/my/book-shelves">
+                    <Button variant="contained" color="primary" size="large" disableElevation>My Shelves</Button>
+                    </Link>
                 </ToolBar>
             </AppBar>
         </div>
