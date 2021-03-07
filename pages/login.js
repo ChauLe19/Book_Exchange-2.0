903e08ddfd91606bcb881efb1113536e67b8b9a3
ChauLe19 <chaule19@vt.edu>
2021-03-07 21:21:44 +0700

    Add Login page

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useState, useEffect } from "react";
+import SignIn from "../components/SignIn";
+
+function Home(props) {
+
+    return (
+        <Fragment>
+            <SignIn/>
+        </Fragment>
+    );
+}
+
+export default Home;
